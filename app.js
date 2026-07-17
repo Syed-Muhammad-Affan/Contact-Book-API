ee20935b8e06ad53d34fbfa8b195afc5a5b76327
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-07-17 17:38:54 +0500

    updating app.js

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ require('express-async-errors');
 
 const connectDB = require('./db/connect');
 const express = require('express');
+const errorHandler = require('./middleware/error-handler');
+const notFound = require('./middleware/not-found');
+
+// Extra Security
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const app = express();
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+// middleware
+app.use(errorHandler());
+app.use(notFound());
 
 const port = process.env.PORT || 3000;
 
