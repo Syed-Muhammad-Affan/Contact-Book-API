804dd01e9ec5c2dfac0d23be8a2a8e3304fd982d
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-07-17 17:35:14 +0500

    adding middleware

---
+++ middleware/error-hanlder.js
@@ -0,0 +1,10 @@
+const { StatusCodes } = require('http-status-codes');
+
+const errorHandler = (err, req, res, next) => {
+  const customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong',
+  };
+
+  res.status(customError.statusCode).json({ msg: customError.msg });
+};
