2f3c0612c816e00e8ee858060b8ebfa28f836f7e
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-07-22 12:35:07 +0500

    adding models schema

---
+++ models/contact.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const contactBookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    maxlength: 100,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: [true, 'Please provide address'],
+  },
+});
+
+module.exports = mongoose.model('ContactBook', contactBookSchema);
