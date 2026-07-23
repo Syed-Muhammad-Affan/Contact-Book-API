6468694fa484095dd34388628e445628003ce76e
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-07-23 13:44:43 +0500

    Update error-handler.js

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,6 +6,28 @@ const errorHandler = (err, req, res, next) => {
     msg: err.message || 'Something went wrong',
   };
 
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+
+    customError.statuscode = 400;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue,
+    )} field, please choose another value`;
+
+    customError.statuscode = 400;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`;
+
+    customError.statuscode = 404;
+  }
+
   res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
